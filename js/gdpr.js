2c9e160392477b2745d0b3528e86d4ca551b969d
FahadUsmanMeer <FahadUsmanMeer>
2024-08-28 00:56:09 +0300

    initial gdpr added

---
+++ js/gdpr.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+    if (!getCookie("gdpr-consent")) {
+        $("#gdpr-banner").show();
+    }
+
+    $("#accept-cookies").click(function (e) {
+        e.preventDefault();
+        var analyticsConsent = $(".analytics-consent").is(":checked");
+        var adsConsent = $(".ads-consent").is(":checked");
+
+        // Store consent in cookies
+        setCookie("gdpr-consent", true, 365);
+        setCookie("analytics-consent", analyticsConsent, 365);
+        setCookie("ads-consent", adsConsent, 365);
+
+        $("#gdpr-banner").hide();
+    });
+});
+
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
