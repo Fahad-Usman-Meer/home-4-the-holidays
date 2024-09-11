c3fdffb78adec28daef6c149e9147395621fc328
FahadUsmanMeer <FahadUsmanMeer>
2024-09-12 01:08:07 +0300

    link 'Bing Tracking' Script

--- js/gdpr2.js
+++ js/gdpr2.js
@@ -45,6 +45,9 @@ function loadAllScripts() {
 
     // Load Google Analytics Com
     loadGoogleAnalyticsComScript();
+
+    // Load Bing Tracking
+    loadBingTrackingScript();
 }
 
 function loadOnlyNecessaryScripts() {
@@ -117,7 +120,41 @@ function loadGoogleAnalyticsComScript() {
     console.log('loaded Google Analytics script id:"loadGoogleAnalyticsComScript"');
 }
 
+function loadBingTrackingScript() {
+    var script = document.createElement('script');
+    script.id = 'loadBingTrackingScript';
+    script.type = 'text/javascript';
+    script.text = `
+                (function (w, d, t, r, u) {
+                    var f, n, i;
+                    w[u] = w[u] || [];
+                    f = function () {
+                        var o = { ti: "5280918" };
+                        o.q = w[u];
+                        w[u] = new UET(o);
+                        w[u].push("pageLoad");
+                    };
+                    n = d.createElement(t);
+                    n.src = r;
+                    n.async = 1;
+                    n.onload = n.onreadystatechange = function () {
+                        var s = this.readyState;
+                        s && s !== "loaded" && s !== "complete" || (f(), n.onload = n.onreadystatechange = null);
+                    };
+                    i = d.getElementsByTagName(t)[0];
+                    i.parentNode.insertBefore(n, i);
+                })(window, document, "script", "//bat.bing.com/bat.js", "uetq");
+            `;
 
+    document.body.appendChild(script);
+
+    console.log('loaded "loadBingTrackingScript"');
+
+    // Create and append the noscript element
+    var noscript = document.createElement('noscript');
+    noscript.innerHTML = '<img src="//bat.bing.com/action/0?ti=5280918&Ver=2" height="0" width="0" style="display: none; visibility: hidden;" />';
+    document.body.appendChild(noscript);
+}
 
 
 
